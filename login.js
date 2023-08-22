b9c0d7dee9559367d7e83c09c90ec79b1429aa82
majba-ul-hoque <majba9399@gmail.com>
2023-08-23 00:21:56 +0600

    location set and create 3 branch

--- login.js
+++ login.js
@@ -15,8 +15,8 @@ document.getElementById('btn-submit').addEventListener('click', function(){
     // step-4 verify email and password
 
     if(email === 'majba@gmail.com' && password === 'secret'){
-        console.log('valid user');
+        window.location.href = 'bank.html'
     }else{
-        console.log('Invalid user!')
+        alert('Invalid user!')
     }
 })
